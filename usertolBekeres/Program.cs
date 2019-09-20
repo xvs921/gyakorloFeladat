320b21767c41ce5cba24a645897e93cef2bc1194
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:18:18 +0200

    hibakezelés

--- usertolBekeres/Program.cs
+++ usertolBekeres/Program.cs
@@ -7,32 +7,36 @@ using System.Threading.Tasks;
 namespace usertolBekeres {
     class Program {
         static void Main(string[] args) {
-            List<int> bekertSzamok = new List<int>();
-            //darabszám
-            Console.WriteLine("Hány darab elemet szeretne megadni?");
-            int elemSzam = Convert.ToInt32(Console.ReadLine());
-            //bekérés
-            for (int i = 0; i < elemSzam; i++) {
-                Console.WriteLine("Kérem adjon meg egy számot!");
-                bekertSzamok.Add( Convert.ToInt32(Console.ReadLine()));
-            }
-            //legnagyobb
-            int legnagyobb = bekertSzamok[0];
-            for (int i = 0; i < bekertSzamok.Count; i++) {
-                if (bekertSzamok[i]>legnagyobb) {
-                    legnagyobb = bekertSzamok[i];
+            try {
+                List<int> bekertSzamok = new List<int>();
+                //darabszám
+                Console.WriteLine("Hány darab elemet szeretne megadni?");
+                int elemSzam = Convert.ToInt32(Console.ReadLine());
+                //bekérés
+                for (int i = 0; i < elemSzam; i++) {
+                    Console.WriteLine("Kérem adjon meg egy számot!");
+                    bekertSzamok.Add(Convert.ToInt32(Console.ReadLine()));
                 }
-            }
-            Console.WriteLine("A legnagyobb szám: " + legnagyobb);
-            //legkisebb
-            int legkisebb = bekertSzamok[0];
-            for (int i = 0; i < bekertSzamok.Count; i++) {
-                if (bekertSzamok[i] < legkisebb) {
-                    legkisebb = bekertSzamok[i];
+                //legnagyobb
+                int legnagyobb = bekertSzamok[0];
+                for (int i = 0; i < bekertSzamok.Count; i++) {
+                    if (bekertSzamok[i] > legnagyobb) {
+                        legnagyobb = bekertSzamok[i];
+                    }
                 }
+                Console.WriteLine("A legnagyobb szám: " + legnagyobb);
+                //legkisebb
+                int legkisebb = bekertSzamok[0];
+                for (int i = 0; i < bekertSzamok.Count; i++) {
+                    if (bekertSzamok[i] < legkisebb) {
+                        legkisebb = bekertSzamok[i];
+                    }
+                }
+                Console.WriteLine("A legkisebb szám: " + legkisebb);
             }
-            Console.WriteLine("A legkisebb szám: " + legkisebb);
-
+            catch (Exception e) {
+                Console.WriteLine("Hiba" + e);
+            }
             Console.ReadLine();
         }
     }
