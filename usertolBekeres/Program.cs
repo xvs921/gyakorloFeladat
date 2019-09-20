958c9dbd44ee81a9735a147660346d3fc2661fb5
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 12:05:40 +0200

    szamok bekerese, legnagyobb kiirasa

--- usertolBekeres/Program.cs
+++ usertolBekeres/Program.cs
@@ -7,8 +7,21 @@ using System.Threading.Tasks;
 namespace usertolBekeres {
     class Program {
         static void Main(string[] args) {
+            List<int> bekertSzamok = new List<int>();
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine("Kérem adjon meg egy számot!");
+                bekertSzamok.Add( Convert.ToInt32(Console.ReadLine()));
+            }
+            int legnagyobb = bekertSzamok[0];
+            for (int i = 0; i < bekertSzamok.Count; i++) {
+                if (bekertSzamok[i]>legnagyobb) {
+                    legnagyobb = bekertSzamok[i];
+                }
+            }
+            Console.WriteLine("A legnagyobb szám: " + legnagyobb);
 
 
+            Console.ReadLine();
         }
     }
 }
